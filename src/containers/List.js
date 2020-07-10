017a26894142619b4803733c8475052946cee822
Petros Shumie <shumie.peter@gmail.com>
2020-07-10 17:51:25 -0400

    updated server json files

--- src/containers/List.js
+++ src/containers/List.js
@@ -4,40 +4,40 @@ import withDataFetching from '../withDataFetching';
 import SubHeader from '../components/Header/SubHeader';
 import ListItem from '../components/ListItem/ListItem';
 
-const ListItemWrapper = styled.div` 
-    display: flex;
-    justify-content: space-between;
-    flex-direction: column;
-    margin: 2% 5%;
-    `;
+const ListItemWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  margin: 2% 5%;
+`;
 
-const Alert = styled.span`  
-    width: 100%;
-    text-align: center;
-    `;
+const Alert = styled.span`
+  width: 100%;
+  text-align: center;
+`;
 
 const List = ({ data, loading, error, match, history }) => {
-    const items =
-    data && data.filter(item => item.listId === parseInt(match.params.id));
+  const items =
+    data && [data].filter(item => item.listId === parseInt(match.params.id));
 
-    return !loading && !error ? (
-        <>
-        {history && (
-            <SubHeader
-            goBack={() => history.goBack()}
-            openForm={() => history.push(`${match.url}/new`)}
-            />
-        )}
-        <ListItemWrapper>
-            {items && items.map(item => <ListItem key={item.id} data={item} />)}
-        </ListItemWrapper>
-        </>
-    ) : (
-        <Alert>{loading ? 'Loading...' : error}</Alert>
-    );
+  return !loading && !error ? (
+    <>
+      {history && (
+        <SubHeader
+          goBack={() => history.goBack()}
+          openForm={() => history.push(`${match.url}/new`)}
+        />
+      )}
+      <ListItemWrapper>
+        {items && items.map(item => <ListItem key={item.id} data={item} />)}
+      </ListItemWrapper>
+    </>
+  ) : (
+    <Alert>{loading ? 'Loading...' : error}</Alert>
+  );
 };
 
 export default withDataFetching({
-    dataSource:
-    'https://my-json-server.typicode.com/pranay/-React-Projects/items',
+  dataSource:
+    'https://my-json-server.typicode.com/shumie-code/react-shopping-list/items',
 })(List);
